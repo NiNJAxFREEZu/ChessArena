4c50c26a4aacb7e9caff36f75b3246e4d94ea076
NiNJAxFREEZu <frugoxmaniak@gmail.com>
2020-06-02 23:50:39 +0200

    You can now exit from tournament creator!!!

--- src/main/java/sample/GUI/TournamentCreatorController.java
+++ src/main/java/sample/GUI/TournamentCreatorController.java
@@ -1,8 +1,26 @@
 package sample.GUI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TournamentCreatorController {
 
+    @FXML
+    public MenuItem menuFileQuit;
+    public Menu menuFile;
+    public MenuBar menuBar;
+    public VBox tournamentCreatorVBox;
+
+    @FXML
+    public void exit(ActionEvent actionEvent) {
+        Stage stage = (Stage) tournamentCreatorVBox.getScene().getWindow();
+        stage.close();
+    }
 }
