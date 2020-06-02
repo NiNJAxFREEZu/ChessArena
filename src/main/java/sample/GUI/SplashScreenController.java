1c3a2022df11cce07f2c3c54e38d2074cca02c46
NiNJAxFREEZu <frugoxmaniak@gmail.com>
2020-06-02 22:43:16 +0200

    Not wired buttons for splash screen

--- src/main/java/sample/GUI/SplashScreenController.java
+++ src/main/java/sample/GUI/SplashScreenController.java
@@ -1,6 +1,8 @@
 package sample.GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +10,23 @@ import org.springframework.stereotype.Component;
 public class SplashScreenController {
 
     @FXML
-    private javafx.scene.control.Button closeButton;
+    public Button newTournamentButton;
+    public Button openButton;
+    public javafx.scene.control.Button closeButton;
 
     @FXML
-    public void closeButtonAction(){
+    public void exit(ActionEvent actionEvent){
         Stage stage = (Stage)closeButton.getScene().getWindow();
         stage.close();
     }
 
+    @FXML
+    public void openFile(ActionEvent actionEvent) {
+        //TODO open file
+    }
+
+    @FXML
+    public void newTournament(ActionEvent actionEvent) {
+        //TODO new tournament
+    }
 }
