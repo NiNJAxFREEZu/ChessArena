6448bc7f3947bc3c79ff89642bfdc890d13b3498
NiNJAxFREEZu <frugoxmaniak@gmail.com>
2020-06-02 22:38:16 +0200

    You can now exit the app!!!

--- src/main/java/sample/GUI/SplashScreenController.java
+++ src/main/java/sample/GUI/SplashScreenController.java
@@ -1,8 +1,19 @@
 package sample.GUI;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SplashScreenController {
+
+    @FXML
+    private javafx.scene.control.Button closeButton;
+
+    @FXML
+    public void closeButtonAction(){
+        Stage stage = (Stage)closeButton.getScene().getWindow();
+        stage.close();
+    }
 
 }
