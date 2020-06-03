0bb502e4c2ae3ecd35e580a4c37f5d41eb9ede7e
kingstiger <piotr.zielinski98@interia.pl>
2020-06-03 19:35:45 +0200

    Enum string values

--- src/main/java/sample/Enums/TournamentType.java
+++ src/main/java/sample/Enums/TournamentType.java
@@ -1,9 +1,16 @@
 package sample.Enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public enum TournamentType {
-    Swiss,
-    RoundRobin,
-    DoubleRoundRobin,
-    HeadsUp,
-    NotSpecified
+    Swiss("Swiss"),
+    RoundRobin("Round Robin"),
+    DoubleRoundRobin("Double Round Robin"),
+    HeadsUp("Heads up"),
+    NotSpecified("Not specified");
+
+    @Getter
+    private String stringValue;
 }
